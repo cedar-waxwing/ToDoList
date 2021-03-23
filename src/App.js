3457f9b5a017b5ebd05288d0ccf6291a81aa8b55
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-03-22 22:18:42 -0400

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
 import React, { Component } from "react";
 import List from "./components/List"
+import Header from "./components/Header"
 
 {/* Want to pass the list below -- the list items -- down into the list itself as a property.*/ }
 class App extends React.Component {
   constructor(props) {
     super(props);
-  {/*anything in this state, react saves as a const. can't push. can create new value and setstate. Create a new versions of array with concat or spred destructuring operator*/}
+    {/*anything in this state, react saves as a const. can't push. can create new value and setstate. Create a new versions of array with concat or spred destructuring operator*/ }
     this.state = { newtodoitem: "", list: [] };
 
-    {/*what is this doing lol*/ }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+    //this.handleKeyPress = this.handleKeyPress.bind(this);
   }
-  handleChange(event) {
+  handleChange = (event) => {
     this.setState({ newtodoitem: event.target.value });
-    console.log(this.state.newtodoitem)
+    //console.log(this.state.newtodoitem)
   }
 
   //this is the updater function for the list on the page 
-  handleSubmit() {
-    this.setState(prevState => ({
-      list: [...prevState.list, {
-        id: this.state.list.length,
-        title: this.state.newtodoitem,
-        completed: false
-      }],
-      newtodoitem: ""
-    }))
-  }
-  
+  handleSubmit = () => {
+    let listCopy = [];
+    let newEntry =
+    {
+      id: this.state.list.length,
+      title: this.state.newtodoitem,
+      completed: false
+    }
+    for (let i = 0; i < this.state.list.length; i++) {
+      if (this.state.list.length > 0) {
+        listCopy.push(this.state.list[i])
+      }
+      console.log(listCopy)
+    }
+    listCopy.push(newEntry)
+    console.log(listCopy)
 
-  render() {
+    this.setState({
+      list: listCopy,
+      newtodoitem: ""
+    });
+  }
+
+  handleKeyPress = (event) => {
+    //console.log(event)
+    if (event.key === "Enter") {
+      this.handleSubmit()
+    }
+  }
+
+  entriesArray = (props) => {
+    const entries = props.entries;
+    const listItems = entries.map((entry) =>
+      <li>{entry}</li>
+    );
+    return (
+      <ul>{listItems}</ul>
+    )
+  }
+
+  render = () => {
     return (
       /* below, this creates the entry field */
-          <>
-            <input type="text" value={this.state.newtodoitem}
-            onChange={this.handleChange} />
-            <button onClick={this.handleSubmit}> New </button>
-          </>
+      <>
+        <Header />
+        {/*how to send data to the List child. This is how to create props:*/}
+         {/* <List listdata={this.state.list}/>  */}
+        <input type="text" value={this.state.newtodoitem}
+          onChange={(e) => this.handleChange(e)} onKeyPress={this.handleKeyPress} />
+        <button onClick={this.handleSubmit}> New </button>
+      </>
+
     );
   }
 }
@@ -45,9 +78,9 @@ class App extends React.Component {
 // state = {
 //   list: [
 //     {
-      // id: 1,
-      // title: "this is the first example",
-      // completed: false
+// id: 1,
+// title: "this is the first example",
+// completed: false
 //     },
 //     {
 //       id: 2,
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+const Header = () => {
+    return (
+ <> <h1> To Do List </h1> </>
+    )
+}
+
+export default Header 
